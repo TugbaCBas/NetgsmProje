7861fa9eb5b3a5bdd2ec2a3e3e715ad455e50eac
Tugba Canoglu <taasnem.jano@ogr.iu.edu.tr>
2021-06-02 07:13:45 +0300

    initial project setup

---
+++ src/main/java/com/tugba/Netgsm/App.java
@@ -0,0 +1,13 @@
+package com.tugba.Netgsm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
